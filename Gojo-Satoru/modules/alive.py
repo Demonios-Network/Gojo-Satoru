17eb502ae20b35c3c16bcef42d7e20a7813a0026
Demonios Network <fowziamujtaba@gmail.com>
2024-09-26 21:19:53 +0530

    Add files via upload

---
+++ Gojo-Satoru/modules/alive.py
@@ -0,0 +1,68 @@
+import asyncio
+from platform import python_version as pyver
+
+from pyrogram import __version__ as pver
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from telegram import __version__ as lver
+from telethon import __version__ as tver
+
+from MukeshRobot import SUPPORT_CHAT, pbot,BOT_USERNAME, OWNER_ID,BOT_NAME,START_IMG
+
+PHOTO = [
+    "https://graph.org/file/8b8c781e18e8597f45f56.jpg",
+    "https://te.legra.ph/file/49059d553efa874c70cb3.jpg",
+    "https://telegra.ph/file/038602f34ce054644b9a9.jpg",
+    "https://te.legra.ph/file/eace7a5d8d4b515a11e50.jpg",
+    "https://te.legra.ph/file/f53a3cd54f7144d0b22cd.jpg",
+]
+
+Mukesh = [
+    [
+        InlineKeyboardButton(text="sʜɪᴠᴀɴsʜ", user_id=OWNER_ID),
+        InlineKeyboardButton(text="ꜱᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{SUPPORT_CHAT}"),
+    ],
+    [
+        InlineKeyboardButton(
+            text="➕ᴀᴅᴅ ᴍᴇ ᴇʟsᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ➕",
+            url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+        ),
+    ],
+]
+
+
+
+@pbot.on_message(filters.command("alive"))
+async def restart(client, m: Message):
+    await m.delete()
+    accha = await m.reply("⚡")
+    await asyncio.sleep(0.2)
+    await accha.edit("ᴅɪɴɢ ᴅᴏɴɢ ꨄ︎ ᴀʟɪᴠɪɴɢ..")
+    await asyncio.sleep(0.1)
+    await accha.edit("ᴅɪɴɢ ᴅᴏɴɢ ꨄ︎ ᴀʟɪᴠɪɴɢ......")
+    await asyncio.sleep(0.1)
+    await accha.edit("ᴅɪɴɢ ᴅᴏɴɢ ꨄ︎ ᴀʟɪᴠɪɴɢ..")
+
+    await accha.delete()
+    await asyncio.sleep(0.3)
+    umm = await m.reply_sticker(
+        "CAACAgUAAxkDAAJHbmLuy2NEfrfh6lZSohacEGrVjd5wAAIOBAACl42QVKnra4sdzC_uKQQ"
+    )
+    await umm.delete()
+    await asyncio.sleep(0.2)
+    await m.reply_photo(
+        START_IMG,
+        caption=f"""**ʜᴇʏ, ɪ ᴀᴍ 『[{BOT_NAME}](f"t.me/{BOT_USERNAME}")』**
+   ━━━━━━━━━━━━━━━━━━━
+  » **ᴍʏ ᴏᴡɴᴇʀ :** [ᴏᴡɴᴇʀ](tg://user?id={OWNER_ID})
+  
+  » **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{lver}`
+  
+  » **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tver}`
+  
+  » **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pver}`
+  
+  » **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{pyver()}`
+   ━━━━━━━━━━━━━━━━━━━""",
+        reply_markup=InlineKeyboardMarkup(Mukesh),
+    )
